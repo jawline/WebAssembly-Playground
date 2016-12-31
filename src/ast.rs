6c7e7c608a654ab5e5a74bcd52c34ffd43b3c5b0
Blake Loring <blake_l@parsed.uk>
2016-12-31 15:21:51 +0000

    Fix some stuff

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,20 @@
 #[derive(Clone, Copy)]
 pub enum Constant {
-	None,
 	Int32(i32)
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum Type {
+	None,
+	Int32
+}
+
+impl ToString for Type {
+	fn to_string(&self) -> String {
+		match self {
+			Int32 => "i32".to_string()
+		}
+	}
 }
 
 #[derive(Clone, Copy)]
@@ -11,28 +24,44 @@ pub enum BinaryOperation {
 
 pub enum AST {
 	Literal(Constant),
-	Function(String, Vec<String>, String, Box<AST>),
+	Function(String, Vec<String>, Box<AST>),
 	BinaryOp(BinaryOperation, Box<AST>, Box<AST>),
 }
 
 impl AST {
 
 	pub fn lit(x: i32) -> Box<AST> {
-		Box::new(AST::Literal(Constant::Int32(5)))
+		Box::new(AST::Literal(Constant::Int32(x)))
 	}
 
 	pub fn add(l: Box<AST>, r: Box<AST>) -> Box<AST> {
 		Box::new(AST::BinaryOp(BinaryOperation::Add, l, r))
 	}
 
+	pub fn as_t(&self) -> Type {
+		match self {
+			&AST::Literal(ref x) => {
+				match *x {
+					Int32 => Type::Int32
+				}
+			},
+			&AST::Function(ref name, ref params, ref body) => {
+				body.as_t()
+			},
+			&AST::BinaryOp(ref op, ref left, ref right) =>
+				match op {
+					Add => if left.as_t() == right.as_t() { left.as_t() } else { Type::None }
+				}
+		}
+	}
+
 	pub fn as_s(&self) -> String {
 		match self {
 			&AST::Literal(ref x) =>
 				match *x {
-					Constant::Int32(v) => ("(i32.const ".to_string() + &v.to_string() + ")").to_string(),
-					Constant::None => "error".to_string()
+					Constant::Int32(v) => ("(i32.const ".to_string() + &v.to_string() + ")").to_string()
 				},
-			&AST::Function(ref name, ref params, ref ret, ref body) => {
+			&AST::Function(ref name, ref params, ref body) => {
 
 				let mut prelude = "(func $".to_string();
 				prelude += &(name.to_string() + " ");
@@ -42,7 +71,7 @@ impl AST {
 					prelude += &("(param $".to_string() + &i.to_string() + " " + &params[i] + ") ");
 				}
 
-				prelude += &("(result ".to_string() + &ret + ") ");
+				prelude += &("(result ".to_string() + &body.as_t().to_string() + ") ");
 				prelude += &body.as_s();
 				prelude += ")";
 				prelude
@@ -50,7 +79,7 @@ impl AST {
 			&AST::BinaryOp(ref op, ref left, ref right) =>
 				match op {
 					Add => {
-						("(i32.add ".to_string() + &left.as_s() + " " + &right.as_s() + ")").to_string()
+						("(".to_string() + &left.as_t().to_string() + ".add " + &left.as_s() + " " + &right.as_s() + ")").to_string()
 					}
 				}
 		}
